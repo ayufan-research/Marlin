65b7a856499a2a8481aec298a4e112a44cd6f34a
Kamil Trzcinski <ayufan@ayufan.eu>
2020-08-26 12:27:23 +0200

    Update E-steps for BMG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -739,7 +739,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 98 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 438.61 }
 
 /**
  * Default Max Feed Rate (mm/s)
