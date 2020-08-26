c2f41a0de52284fb80f6a1f1a47ca5f73cc4f3ed
Kamil Trzcinski <ayufan@ayufan.eu>
2020-08-26 12:28:15 +0200

    Update probe offset for custom HeroGenMe5 mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -985,7 +985,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41, -15, -0.3 }
+#define NOZZLE_TO_PROBE_OFFSET { -55, -23, -0.97 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
