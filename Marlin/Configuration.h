811d2a0f4e92fb294c89f6e0b06592d6e7814eab
Kamil Trzcinski <ayufan@ayufan.eu>
2020-09-24 21:22:12 +0200

    Change microsteps to 32

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -739,7 +739,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 438.61 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1600, 880 }
 
 /**
  * Default Max Feed Rate (mm/s)
