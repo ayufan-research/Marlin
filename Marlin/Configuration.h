71908509d35c8dab48b28c3525d6e07f57f13bd1
Kamil Trzcinski <ayufan@ayufan.eu>
2020-09-24 21:58:37 +0200

    Change Z to 16 microsteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -739,7 +739,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1600, 880 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 880 }
 
 /**
  * Default Max Feed Rate (mm/s)
