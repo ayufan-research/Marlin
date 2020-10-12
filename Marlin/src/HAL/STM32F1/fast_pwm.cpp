79e85e3cd0aee71195e4167a78585434f8e08ee6
Kamil Trzcinski <ayufan@ayufan.eu>
2020-10-12 19:54:12 +0200

    Debug messages for FastPWM

--- Marlin/src/HAL/STM32F1/fast_pwm.cpp
+++ Marlin/src/HAL/STM32F1/fast_pwm.cpp
@@ -2,6 +2,7 @@
 
 #include "../../inc/MarlinConfigPre.h"
 #include "../../MarlinCore.h"
+#include "../../core/debug_out.h"
 
 #if NEEDS_HARDWARE_PWM // Specific meta-flag for features that mandate PWM
 
@@ -30,14 +31,14 @@ void set_pwm_frequency(const pin_t pin, int f_desired) {
   timer_set_prescaler(dev, (uint16)(prescaler - 1));
   timer_set_reload(dev, overflow);
 
-  SERIAL_ECHO_START();
-  SERIAL_ECHOPGM("set_pwm_frequency:");
-  SERIAL_ECHOPAIR_F(" pin:", pin);
-  SERIAL_ECHOPAIR_F(" f_desired:", f_desired);
-  SERIAL_ECHOPAIR_F(" period_cyc:", period_cyc);
-  SERIAL_ECHOPAIR_F(" prescaler:", prescaler);
-  SERIAL_ECHOPAIR_F(" overflow:", overflow);
-  SERIAL_EOL();
+  DEBUG_ECHO_START();
+  DEBUG_ECHOPGM("set_pwm_frequency:");
+  DEBUG_ECHOPAIR_F(" pin:", pin);
+  DEBUG_ECHOPAIR_F(" f_desired:", f_desired);
+  DEBUG_ECHOPAIR_F(" period_cyc:", period_cyc);
+  DEBUG_ECHOPAIR_F(" prescaler:", prescaler);
+  DEBUG_ECHOPAIR_F(" overflow:", overflow);
+  DEBUG_EOL();
 }
 
 void set_pwm_duty(const pin_t pin, const uint16_t v, const uint16_t v_size/*=255*/, const bool invert/*=false*/) {
@@ -50,23 +51,23 @@ void set_pwm_duty(const pin_t pin, const uint16_t v, const uint16_t v_size/*=255
     return;
   }
 
+  // Recv: echo:set_pwm_frequency: pin:38 v:0 v_size:255 invert:0
+  // Recv: echo:set_pwm_frequency: pin:38 v:0 v_size:255 invert:0
+
   uint8 cc_channel = PIN_MAP[pin].timer_channel;
   uint16 reload = timer_get_reload(dev);
   uint16 duty_cycle = (invert ? 1.0f - (float)v / v_size : (float)v / v_size) * reload;
   timer_set_compare(dev, cc_channel, duty_cycle);
 
-  SERIAL_ECHO_START();
-  SERIAL_ECHOPGM("set_pwm_frequency:");
-  SERIAL_ECHOPAIR_F(" pin:", pin);
-  SERIAL_ECHOPAIR_F(" v:", v);
-  SERIAL_ECHOPAIR_F(" v_size:", v_size);
-  SERIAL_ECHOPAIR_F(" invert:", invert);
-  SERIAL_ECHOPAIR_F(" reload:", reload);
-  SERIAL_ECHOPAIR_F(" duty_cycle:", duty_cycle);
-  SERIAL_EOL();
-
-  // Recv: echo:set_pwm_frequency: pin:38 v:0 v_size:255 invert:0
-  // Recv: echo:set_pwm_frequency: pin:38 v:0 v_size:255 invert:0
+  DEBUG_ECHO_START();
+  DEBUG_ECHOPGM("set_pwm_frequency:");
+  DEBUG_ECHOPAIR_F(" pin:", pin);
+  DEBUG_ECHOPAIR_F(" v:", v);
+  DEBUG_ECHOPAIR_F(" v_size:", v_size);
+  DEBUG_ECHOPAIR_F(" invert:", invert);
+  DEBUG_ECHOPAIR_F(" reload:", reload);
+  DEBUG_ECHOPAIR_F(" duty_cycle:", duty_cycle);
+  DEBUG_EOL();
 }
 
 #endif // NEEDS_HARDWARE_PWM
